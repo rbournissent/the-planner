6bf6e027d6d8e04af0a32ff7c9bf5e689bef7f70
Rodrigo Bournissent <rodrigobourni@hotmail.com>
2023-10-03 12:32:01 +0200

    Add shiftsReducer
    And update state to include shifts
    Add shifts selector

--- src/app/state/reducers/index.ts
+++ src/app/state/reducers/index.ts
@@ -7,17 +7,23 @@ import { Employee } from 'src/app/models/employee.model';
 import { employeesReducer } from './employees.reducer';
 import { ShiftTemplate } from 'src/app/models/shift-template.model';
 import { templatesReducer } from './shift-templates.reducer';
+import { Shift } from 'src/app/models/shift.model';
+import { shiftsReducer } from './shifts.reducer';
 
 export interface State {
   employees: ReadonlyArray<Employee>
   templates: ReadonlyArray<ShiftTemplate>
+  shifts: ReadonlyArray<Shift>
 }
 
 export const reducers: ActionReducerMap<State> = {
   employees: employeesReducer,
   templates: templatesReducer,
+  shifts: shiftsReducer,
 };
 
 export const selectEmployees = createFeatureSelector<ReadonlyArray<Employee>>('employees');
 
 export const selectTemplates = createFeatureSelector<ReadonlyArray<ShiftTemplate>>('templates');
+
+export const selectShifts = createFeatureSelector<ReadonlyArray<Shift>>('shifts');
