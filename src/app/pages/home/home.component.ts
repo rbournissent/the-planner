62333fd4bfd658ad6bd175c9307502563415df08
Rodrigo Bournissent <rodrigobourni@hotmail.com>
2023-10-03 14:04:47 +0200

    Add button to open modal in calendar
    Define openShiftForm method to open and close modal
    Save shift on submit

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,9 +3,12 @@ import { CalendarOptions, EventInput } from '@fullcalendar/core';
 import interactionPlugin from '@fullcalendar/interaction'
 import timeGridPlugin from '@fullcalendar/timegrid'
 import { Subscription, BehaviorSubject } from 'rxjs';
+import { Overlay, OverlayRef } from '@angular/cdk/overlay'
+import { ComponentPortal } from '@angular/cdk/portal'
 
 import { CALENDAR_OPTIONS } from './../../consts';
 import { ShiftService } from 'src/app/services/shift.service';
+import { ShiftFormComponent } from 'src/app/components/shift-form/shift-form.component';
 
 @Component({
   selector: 'app-home',
@@ -15,13 +18,27 @@ import { ShiftService } from 'src/app/services/shift.service';
 export class HomeComponent implements OnDestroy {
   shiftsSub?: Subscription;
   events = new BehaviorSubject<EventInput[]>([])
+  overlayRef: OverlayRef
 
-  constructor (private shiftService: ShiftService) {
+  constructor (
+    private shiftService: ShiftService,
+    private overlay: Overlay
+  ) {
     this.shiftsSub = this.shiftService.shifts$.subscribe(shifts => {
       // TODO: Evaluate filtering shifts by _args.start and _args.end
       this.events.next(shifts.map(this.shiftService.shiftAsEventInput))
     })
+
+    // Create overlay centered in the screen (with backdrop)
+    this.overlayRef = this.overlay.create({
+      positionStrategy: this.overlay.position().global()
+        .centerHorizontally()
+        .centerVertically(),
+      hasBackdrop: true
+    })
   }
+
+
 
   calendarOptions: CalendarOptions = {
     ...CALENDAR_OPTIONS,
@@ -29,6 +46,7 @@ export class HomeComponent implements OnDestroy {
       interactionPlugin,
       timeGridPlugin
     ],
+    // TODO: Validate shift change on drop or remove functionality
     eventAllow: (droppedSpan, event) => {
       if (!event || !event.start || !event.end) return false
 
@@ -37,12 +55,44 @@ export class HomeComponent implements OnDestroy {
       return droppedSpan.start.toTimeString() === start.toTimeString() &&
         droppedSpan.end.toTimeString() === end.toTimeString()
     },
+    // Add Schedule Shift button in the footer toolbar
+    customButtons: {
+      scheduleShift: {
+        text: 'Schedule Shift',
+        click: this.openShiftForm.bind(this)
+      }
+    },
+    footerToolbar: {
+      right: 'scheduleShift'
+    },
   };
 
   removeShift(id?: string) {
     if (id) {
       this.shiftService.removeShift(parseInt(id))
     }
+  }
+
+  openShiftForm () {
+    const form = new ComponentPortal(ShiftFormComponent)
+    const formRef = this.overlayRef.attach(form)
+    const subs: Subscription[] = []
+
+    // Close modal
+    subs.push(formRef.instance.closed.subscribe(isClosed => {
+      if (isClosed) {
+        subs.forEach(sub => sub.unsubscribe())
+        this.overlayRef.detach()
+      }
+    }))
+    // Save shift and close on submit
+    subs.push(formRef.instance.submittedData.subscribe(shift => {
+      if (shift) {
+        this.shiftService.saveShift(shift)
+        subs.forEach(sub => sub.unsubscribe())
+        this.overlayRef.detach()
+      }
+    }))
   }
 
   ngOnDestroy () {
