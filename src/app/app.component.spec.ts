64cd069668c63dec677793d50ed4dd5b85751550
Rodrigo Bournissent <rodrigobourni@hotmail.com>
2023-10-03 09:56:31 +0200

    Fix unit tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,17 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'the-planner'`, () => {
+  it(`should have as title 'The Planner'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('the-planner');
+    expect(app.title).toEqual('The Planner');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('the-planner app is running!');
+    expect(compiled.querySelector('h1')?.textContent)
+      .toContain('The Planner');
   });
 });
