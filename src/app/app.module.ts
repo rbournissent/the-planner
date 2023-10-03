6199b7bb305b14499419f3b0f2cf884f621043c6
Rodrigo Bournissent <rodrigobourni@hotmail.com>
2023-10-03 13:56:56 +0200

    Create ShiftForm component
    Form to be used as a modal to schedule (create) a new Shift

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { FullCalendarModule } from '@fullcalendar/angular';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { OverlayModule } from '@angular/cdk/overlay'
+import { PortalModule } from '@angular/cdk/portal';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { reducers } from './state/reducers';
 import { SvgIconComponent } from './components/svg-icon/svg-icon.component';
 import { EventComponent } from './components/event/event.component';
+import { ShiftFormComponent } from './components/shift-form/shift-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     SvgIconComponent,
-    EventComponent
+    EventComponent,
+    ShiftFormComponent
   ],
   imports: [
     BrowserModule,
@@ -25,7 +30,10 @@ import { EventComponent } from './components/event/event.component';
     StoreModule.forRoot(reducers),
     StoreDevtoolsModule.instrument({
       maxAge: 25
-    })
+    }),
+    OverlayModule,
+    PortalModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
