19fb05d65b4f9a907604fa32e7c1ed29d9e910e1
Rodrigo Bournissent <rodrigobourni@hotmail.com>
2023-10-03 11:33:42 +0200

    Add template reducer and selector to reducers

--- src/app/state/reducers/index.ts
+++ src/app/state/reducers/index.ts
@@ -5,13 +5,19 @@ import {
 
 import { Employee } from 'src/app/models/employee.model';
 import { employeesReducer } from './employees.reducer';
+import { ShiftTemplate } from 'src/app/models/shift-template.model';
+import { templatesReducer } from './shift-templates.reducer';
 
 export interface State {
   employees: ReadonlyArray<Employee>
+  templates: ReadonlyArray<ShiftTemplate>
 }
 
 export const reducers: ActionReducerMap<State> = {
   employees: employeesReducer,
+  templates: templatesReducer,
 };
 
 export const selectEmployees = createFeatureSelector<ReadonlyArray<Employee>>('employees');
+
+export const selectTemplates = createFeatureSelector<ReadonlyArray<ShiftTemplate>>('templates');
